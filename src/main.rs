4ee4895535f14e4780e5ed00069944480df8c851
Pavitra Golchha <hello@pavi2410.me>
2022-07-22 22:35:30 +0530

    Lexer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::io::Write;
 use std::io::stdin;
 use std::io::stdout;
+use std::io::Write;
 
 /*
    GRAMMAR
@@ -27,60 +27,63 @@ fn main() {
             break;
         }
 
-        let tokens = tokenize(input);
-        // println!("{:?}", tokens);
-        for t in tokens {
-            println!("Token({:?}, {})", t.kind, t.val);
-        }
+        let tokens = lex(input);
+
+        println!("{:?}", tokens);
+
         // let ast = parse(tokens);
         // let output = eval(ast);
     }
 }
 
-fn prompt(name:&str) -> String {
+fn prompt(name: &str) -> String {
     let mut line = String::new();
     print!("{}", name);
     stdout().flush().unwrap();
-    stdin().read_line(&mut line).expect("Error: Could not read a line");
- 
+    stdin()
+        .read_line(&mut line)
+        .expect("Error: Could not read a line");
+
     line.trim().to_string()
 }
 
-#[derive(Debug)]
-enum TokenType {
-    number,
-    symbol,
-    string,
-    comment,
-    lpar, rpar,
-    lbrace, rbrace,
+#[derive(Debug, PartialEq)]
+enum Token {
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    Number(i32),
+    String(String),
+    Symbol(String),
 }
 
-struct Token<'a> {
-    kind: TokenType,
-    val: &'a str
-}
-
-fn tokenize(input: String) -> Vec<Token<'static>> {
+fn lex(input: String) -> Vec<Token> {
     let mut tokens = Vec::new();
-    let mut buf = String::new();
 
-    use TokenType::*;
+    let sanitized_input = &input
+        .replace("(", " ( ")
+        .replace(")", " ) ")
+        .replace("{", " { ")
+        .replace("}", " } ");
 
-    for c in input.chars() {
-        buf.push(c);
-        let token = match buf.as_str() {
-            "(" => Some(Token { kind: lpar, val: "(" }),
-            ")" => Some(Token { kind: rpar, val: ")" }),
-            "{" => Some(Token { kind: rpar, val: "{" }),
-            "}" => Some(Token { kind: rpar, val: "}" }),
-            "ab" => Some(Token { kind: symbol, val: "ab" }),
-            _ => None
+    for word in sanitized_input.split_ascii_whitespace() {
+        let token = match word {
+            "(" => Some(Token::LParen),
+            ")" => Some(Token::RParen),
+            "{" => Some(Token::LBrace),
+            "}" => Some(Token::RBrace),
+            _ => {
+                if let Ok(num) = word.parse::<i32>() {
+                    Some(Token::Number(num))
+                } else if word.starts_with('"') {
+                    Some(Token::String(word[1..word.len() - 1].to_string()))
+                } else {
+                    Some(Token::Symbol(word.to_string()))
+                }
+            }
         };
-        if let Some(token) = token {
-            tokens.push(token);
-            buf.clear();
-        }
+        tokens.push(token.unwrap());
     }
 
     tokens
@@ -92,9 +95,61 @@ fn parse() {
 
 #[cfg(test)]
 mod tests {
+    use crate::lex;
+    use crate::Token::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_lexer() {
+        let result = lex("(+ 2 24)".to_string());
+        assert_eq!(
+            result,
+            vec![
+                LParen,
+                Symbol("+".to_string()),
+                Number(2),
+                Number(24),
+                RParen
+            ]
+        );
+    }
+
+    #[test]
+    fn test_lexer2() {
+        let result = lex(r#"
+        (fun {areacircle rad} { do
+            (print "Radius:\t" rad)
+            (print "Area:\t" (* rad rad))
+        })
+        "#
+        .to_string());
+        assert_eq!(
+            result,
+            vec![
+                LParen,
+                Symbol("fun".to_string()),
+                LBrace,
+                Symbol("areacircle".to_string()),
+                Symbol("rad".to_string()),
+                RBrace,
+                LBrace,
+                Symbol("do".to_string()),
+                LParen,
+                Symbol("print".to_string()),
+                String("Radius:\\t".to_string()),
+                Symbol("rad".to_string()),
+                RParen,
+                LParen,
+                Symbol("print".to_string()),
+                String("Area:\\t".to_string()),
+                LParen,
+                Symbol("*".to_string()),
+                Symbol("rad".to_string()),
+                Symbol("rad".to_string()),
+                RParen,
+                RParen,
+                RBrace,
+                RParen
+            ]
+        );
     }
 }
